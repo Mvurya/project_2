efe22c55ec4533fbac0bb16247bc1a0d197af89f
Hassan Mvurya <hassan.mvurya@strathmore.edu>
2024-07-01 17:45:10 +0300

    new pages

---
+++ loops.php
@@ -0,0 +1,54 @@
+<h1>loops</h1>
+<?php
+// While-loop
+
+$in = 0;
+while($in < 10){
+    print $in . "<br>";
+    $in++;
+}
+?>
+<?php
+//do while loop
+
+$s = 5;
+do{
+    print $s . "<br>";
+    $s++;
+}while($s < 15);
+?>
+<h4>for loop</h4>
+<?php
+for($f=45; $f<55; $f++) {
+    print $f . "<br>";
+}
+?>
+<h4>Foreach</h4>
+<?php
+$months = ["January", "February", "March", "April",
+"May", "June", "July", "August", "September", "October", "November", "December"];
+foreach($months AS $month){
+    print $month . "<br>";
+}
+?>
+
+<form action="">
+    <select name="" id="">
+        <option value="">--Months--</option>
+        <?php
+        foreach($months AS $month){
+            print "<option value=''>$month</option";
+        }
+        ?>
+        </select>
+        <select name="" id="">
+            <option value="">--Year--</option>
+            <?php
+            //while loop
+            $y = 2014;
+            while($y < 2030){
+                print "<option value=''>$y</option>";
+            }
+            ?>
+            </select>
+    </form>
