f116edc9addcc8901b7b277b63f38f50cd5863da
Hassan Mvurya <hassan.mvurya@strathmore.edu>
2024-07-10 22:11:03 +0300

    adding footer

--- index2.php
+++ index2.php
@@ -12,12 +12,20 @@
 <h1>ABOUT</h1>
 </div>
 <div class="content">
-    <p>The New Kingdom was preceded by the Middle Kingdom (around 2055-1650 BC)<br>
-         and Old Kingdom (around 2686-2181 BC). The Old Kingdom is famous for its pyramids,<br>
-          including the Great Pyramid of Giza, built for Pharaoh Khufu.<br>
-           During this time, Egypt’s central administration became well-developed, enabling major<br>
-            advances in architecture, art, and technology.</p>
+    <p>Established in 2005, All Specs was founded by a team of passionate individuals who shared a common goal<br>
+     – to make eyewear shopping a delightful experience.<br>
+      With years of expertise in the optical industry,<br>
+       we have curated a collection of spectacles that blend innovation, comfort, and aesthetics seamlessly.</p>
+       
+       
+       <p> We take pride in the quality of our products.<br>
+        Each pair of spectacles undergoes rigorous testing to ensure durability, comfort, and optimal vision correction.<br>
+         Our commitment to excellence drives us to source materials from trusted<br>
+          suppliers and collaborate with skilled artisans to craft eyewear that meets the highest standards.</p>
             </div>
             <?php include_once("templates/sidebar.php"); ?>
+            <div class="footer">
+        copyright CONSTRUCTION CO 2024 &copy; All rights reserved
+    </div>
 
 </body>
