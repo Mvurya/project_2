13d21e163bcc337ab1c78287625f6d1ce65ad0cb
Hassan Mvurya <hassan.mvurya@strathmore.edu>
2024-07-11 00:16:00 +0300

    editing footer

--- index5.php
+++ index5.php
@@ -29,7 +29,7 @@
     <?php include_once("templates/sidebar.php"); ?>
 
     <div class="footer">
-        copyright CONSTRUCTION CO 2024 &copy; All rights reserved
+        copyright ALL SPECS CO 2024 &copy; All rights reserved
     </div>
 
 
