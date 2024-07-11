54505ae1d3f23c6d690706fdc0f3d5f85deb979a
Hassan Mvurya <hassan.mvurya@strathmore.edu>
2024-07-11 10:57:22 +0300

    adding footer

--- index.php
+++ index.php
@@ -14,11 +14,11 @@
     </div>
 <div class="row:after">
     <div class="content">
-    <p>Welcome to all spects, where we bring clarity to your world through our premium eyewear solutions. <br>
-       At all spects, we believe that seeing is not just about vision; it’s about experiencing life in vivid detail. <br>
+    <p>Welcome to ALL SPECS, where we bring clarity to your world through our premium eyewear solutions. <br>
+       At ALL SPECS, we believe that seeing is not just about vision; it’s about experiencing life in vivid detail. <br>
         Our mission is to provide you with stylish and innovative spectacles that not only enhance your vision but <br>
         also reflect your unique style. With a commitment to quality, comfort, and cutting-edge design, we invite you <br>
-         to explore our collection and discover the perfect frames for your individuality. See the world differently with all spects.
+         to explore our collection and discover the perfect frames for your individuality. See the world differently with ALL SPECS.
 
 </p>
 
